59dd93dbe75d1584a46b9cb753c547d2788e8e88
cheluvesha <cheluvesh.b@gmail.com>
2020-09-10 21:22:24 +0530

    [CHELUVESHA]Modified:Modified checkNoplaySnakeLadder function and Added Win condition

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,6 +2,8 @@
 import java.util.Random;
 class SnakeAndLadder {
 
+	final static int STEP = 3;
+	
 	// welcome to starting of the Game	
 	private static void welcomeToGame() {
 		System.out.println("Player will Start from position 0");
@@ -16,18 +18,33 @@ class SnakeAndLadder {
 	
 	private static void checkNoplayLadderSnake() {
 		Random random = new Random();
-		int option = random.nextInt(3);
-		int moveDie = rollADie();
+		int win = 0;
+		while( win < 100 ) {
 			
-		// On the basis of Option Player Position will take place
-		if(option == 0) {
-			System.out.println("Player its a foult so no changes to the current position");
-		}
-		else if(option == 2) {
-			System.out.println("oops!! Player you step on a snake so you will go back to 3 step back");
-		}
-		else {
-			System.out.println("Congrats!! Player you advanced to: "+ moveDie +" positions");
+			int option = random.nextInt(3);
+			int moveDie = rollADie();
+				
+			// On the basis of Option Player Position will take place
+			if(option == 0) {
+				System.out.println("Player its a foult so no changes to the current position \n Current position is: "+win);
+			}
+			// condition win greater than roll die value to avoid negative value
+			else if(option == 2 && win > moveDie) {
+				System.out.println("oops!! Player you step on a snake so you will go back to "+STEP+"  step back");
+				win -= STEP;
+			}
+			else {
+				win += moveDie;
+				if( win > 100 )
+					win -= moveDie;
+					
+				else
+					System.out.println("Congrats!! Player you advanced to: "+ moveDie +" positions");
+					System.out.println(" Player currently you ended at: "+ win +" position");
+			}
+			// condition to check whether its at 100
+			if( win == 100 )
+				System.out.println("Congrats you have Reached 100th Position"); 
 		}
 	} 
 
