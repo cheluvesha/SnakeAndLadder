479993d7ffc4d69728dbfddfb50982d58230e274
cheluvesha <cheluvesh.b@gmail.com>
2020-09-10 20:26:24 +0530

    [CHELUVESHA]Added: Declared Starting point

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,3 +1,12 @@
 // Welcome to Snake And Ladder Game
 
+class SnakeAndLadder {
 
+	// welcome to starting of the Game	
+	private static void welcomeToGame() {
+		System.out.println("Player will Start from position 0");
+	}
+	public static void main(String args[]) {
+		welcomeToGame();
+	}
+}
