89d6caab6a34d4ff38214d24d18425161071162b
cheluvesha <cheluvesh.b@gmail.com>
2020-09-10 20:33:33 +0530

    [CHELUVESHA]Added: Roll a Die function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,22 @@
 // Welcome to Snake And Ladder Game
-
+import java.util.Random;
 class SnakeAndLadder {
 
 	// welcome to starting of the Game	
 	private static void welcomeToGame() {
 		System.out.println("Player will Start from position 0");
 	}
+	
+	private static void rollADie() {
+		Random random = new Random();
+		int dieValue = random.nextInt(6) + 1;
+		System.out.println("The die value is: "+ dieValue);
+	}
+
 	public static void main(String args[]) {
+	
 		welcomeToGame();
+	 	rollADie();	
+
 	}
 }
