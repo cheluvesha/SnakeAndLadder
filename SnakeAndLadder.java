2f4bd7ac754184b0c3050d5bf057a6b279ac8880
cheluvesha <cheluvesh.b@gmail.com>
2020-09-11 18:09:47 +0530

    [CHELUVESHA]Added: noOfTimeDieRolled Variable

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -28,6 +28,7 @@ class SnakeAndLadder {
 		
 		Random random = new Random();
 		int win = 0;// position
+		int noOfTimeDieRolled = 0;
 		
 		// to repeat till 100th position
 		while( win < WIN_GOAL ) {
@@ -40,15 +41,19 @@ class SnakeAndLadder {
 			if(option == NO_PLAY) {
 				
 				System.out.println("Player its a foult so no changes to the current position \n Current position is: "+win);
-			
+				noOfTimeDieRolled++;
+
 			}
 			
 			// condition win greater than roll die value to avoid negative value
 			else if(option == SNAKE) {
+
+				noOfTimeDieRolled++;
 				
 				if(win > moveDie) {
 					
 					win -= moveDie;
+					noOfTimeDieRolled++;
 				
 				}
 			
@@ -60,22 +65,28 @@ class SnakeAndLadder {
 
 				System.out.println("oops!! Player you step on a snake so you will go back to "+moveDie+
 				" step back Or Position will start from O Position");
-			
+				System.out.println(" Player currently you ended at in: "+ win +" position");
+
 			}
 			
 			else {
 
 				win += moveDie;
+				noOfTimeDieRolled++;
+	
+				// if win reached more than 100
 				if( win > WIN_GOAL ) {
 					
 					win -= moveDie;
+					System.out.println(" Player currently you ended at in: "+ win +" position please get a lesser value from the dice");
 				
 				}
-				
-				else {
 					
+				else {
+
 					System.out.println("Congrats!! Player you advanced to: "+ moveDie +" positions");
-					System.out.println(" Player currently you ended at: "+ win +" position");
+					System.out.println(" Player currently you ended at in: "+ win +" position");
+				
 				}
 				
 			}
@@ -84,10 +95,13 @@ class SnakeAndLadder {
 			if( win == WIN_GOAL ) {
 				
 				System.out.println("Congrats you have Reached 100th Position"); 
-				
+			
 			}
 			
 		}
+			
+			System.out.println("The total number of dice rolled is: "+noOfTimeDieRolled);
+	
 	} 
 
 	public static void main(String args[]) {
