fa315f98daf4fb6a61d33b56af7b17e4fecdbecc
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-11 12:50:28 +0530

    [CHELUVESHA]Modified: Followed Coding Intentions.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,13 +4,17 @@ class SnakeAndLadder {
 
 	// welcome to starting of the Game	
 	private static void welcomeToGame() {
+		
 		System.out.println("Player will Start from position 0");
+	
 	}
 	
 	private static void rollADie() {
+		
 		Random random = new Random();
 		int dieValue = random.nextInt(6) + 1;
 		System.out.println("The die value is: "+ dieValue);
+	
 	}
 
 	public static void main(String args[]) {
