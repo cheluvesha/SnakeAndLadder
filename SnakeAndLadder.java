036dde7cd05e5a6f7a7c02de461d1f341383710b
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-11 14:29:21 +0530

    [CHELUVESHA]Refactor: followed Coding Intentions.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -50,20 +50,27 @@ class SnakeAndLadder {
 			
 			else {
 				win += moveDie;
-				if( win > 100 )
-					win -= moveDie;
+				if( win > 100 ) {
 					
-				else
+					win -= moveDie;
+				
+				}
+				
+				else {
+					
 					System.out.println("Congrats!! Player you advanced to: "+ moveDie +" positions");
 					System.out.println(" Player currently you ended at: "+ win +" position");
-			
+				}
+				
 			}
 			
 			// condition to check whether its at 100
-			if( win == 100 )
+			if( win == 100 ) {
 				
 				System.out.println("Congrats you have Reached 100th Position"); 
-		
+				
+			}
+			
 		}
 	} 
 
