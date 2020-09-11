93b6e69ba722bbeacbfac4d68f42bebae544e4a8
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-11 17:40:21 +0530

    [CHELUVESHA]WIP: need to work a little bit

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,96 +2,98 @@
 import java.util.Random;
 class SnakeAndLadder {
 
-	static final int STEP = 3;
-	static final int SNAKE = 2;
-	static final int NO_PLAY = 0;
+	final static int SNAKE = 2;
+	final static int NO_PLAY = 0;
+	final static int WIN_GOAL = 100;
 
 	// welcome to starting of the Game	
 	private static void welcomeToGame() {
-
+		
 		System.out.println("Player will Start from position 0");
-		System.out.println("<------------------------------------------------------->");
-
+	
 	}
 	
 	// roll a die through random number generation
-	private static int rollADie() {
-
+	private static int rollADie() { 
+		
 		Random random = new Random();
 		int dieValue = random.nextInt(6) + 1;
 		System.out.println("The rolled die value is: "+ dieValue);
 		return dieValue;
-
+	
 	}
 	
-	private static void getToExactWinPosition() {
-
+	// method to repeat till its reach 100
+	private static void repeatTillPlayerReachGoal() {
+		
 		Random random = new Random();
-		int win = 0;// Position
-		int noOfTimeDieRolled = 0;
+		int win = 0;// position
 		
 		// to repeat till 100th position
-		while( win < 100 ) {
+		while( win < WIN_GOAL ) {
 			
 			int option = random.nextInt(3);
 			int moveDie = rollADie();
-				
+			System.out.println(" option choice: "+option);			
+	
 			// On the basis of Option Player Position will take place
 			if(option == NO_PLAY) {
-
+				
 				System.out.println("Player its a foult so no changes to the current position \n Current position is: "+win);
-				noOfTimeDieRolled++;
 			
 			}
-
+			
 			// condition win greater than roll die value to avoid negative value
-			else if(option == SNAKE && win > moveDie) {
-
-				System.out.println("oops!! Player you step on a snake so you will go back to "+STEP+"  step back");
-				win -= STEP;
-				noOfTimeDieRolled++;
-				System.out.println("Current position is: "+win);
-
-			}
-
-			else {
-				win += moveDie;
+			else if(option == SNAKE) {
 				
-				//to stop player exact in 100	
-				if( win > 100 ) {
-
-					win -= moveDie;
-					noOfTimeDieRolled++;
+				if(win > moveDie) {
 					
+					win -= moveDie;
+				
+				}
+			
+				else {
+				
+					win = 0;
+				
 				}
 
-				else {
-
-					System.out.println("Congrats!! Player you advanced to: "+ moveDie +" positions");
-					System.out.println(" Player currently you ended at: "+ win +" position");
-					noOfTimeDieRolled++;
-				
-				}	
-			}
-
-			// condition to check whether its at 100
-			if( win == 100 ) {
-
-				System.out.println("Congrats you have Reached 100th Position"); 
+				System.out.println("oops!! Player you step on a snake so you will go back to "+moveDie+
+				" step back Or Position will start from O Position");
 			
 			}
+			
+			else {
 
+				win += moveDie;
+				if( win > WIN_GOAL ) {
+					
+					win -= moveDie;
+				
+				}
+				
+				else {
+					
+					System.out.println("Congrats!! Player you advanced to: "+ moveDie +" positions");
+					System.out.println(" Player currently you ended at: "+ win +" position");
+				}
+				
+			}
+			
+			// condition to check whether its at 100
+			if( win == WIN_GOAL ) {
+				
+				System.out.println("Congrats you have Reached 100th Position"); 
+				
+			}
+			
 		}
-
-		System.out.println("<------------------------------------------------------->");		
-		System.out.println("The Number of times Dice rolled is : "+noOfTimeDieRolled);
-
 	} 
 
 	public static void main(String args[]) {
-
+		
 		welcomeToGame();
-	 	getToExactWinPosition();	
-
+	 	repeatTillPlayerReachGoal();
+		
 	}
 }
