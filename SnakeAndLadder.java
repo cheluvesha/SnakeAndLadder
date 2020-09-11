9eab336c81ed27431aacc62bfb84c0ea8b4ff89f
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-11 12:49:59 +0530

    [CHELUVESHA]Modified: Followed Coding Intentions.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,9 +4,14 @@ class SnakeAndLadder {
 
 	// welcome to starting of the Game	
 	private static void welcomeToGame() {
+		
 		System.out.println("Player will Start from position 0");
+	
 	}
+	
 	public static void main(String args[]) {
+		
 		welcomeToGame();
+	
 	}
 }
