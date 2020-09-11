2c29612a06e285f18fdf158bd374cebd7b3ab00a
cheluvesha <cheluvesh.b@gmail.com>
2020-09-11 17:17:42 +0530

    [CHELUVESHA]Refactor: to Suit UC statement

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,9 +2,9 @@
 import java.util.Random;
 class SnakeAndLadder {
 
-	final static int STEP = 3;
 	final static int SNAKE = 2;
 	final static int NO_PLAY = 0;
+	final static int WIN_GOAL = 100;
 
 	// welcome to starting of the Game	
 	private static void welcomeToGame() {
@@ -30,11 +30,12 @@ class SnakeAndLadder {
 		int win = 0;// position
 		
 		// to repeat till 100th position
-		while( win < 100 ) {
+		while( win < WIN_GOAL ) {
 			
 			int option = random.nextInt(3);
 			int moveDie = rollADie();
-				
+			System.out.println(" option choice: "+option);			
+	
 			// On the basis of Option Player Position will take place
 			if(option == NO_PLAY) {
 				
@@ -43,16 +44,29 @@ class SnakeAndLadder {
 			}
 			
 			// condition win greater than roll die value to avoid negative value
-			else if(option == SNAKE && win > moveDie) {
+			else if(option == SNAKE) {
 				
-				System.out.println("oops!! Player you step on a snake so you will go back to "+STEP+"  step back");
-				win -= STEP;
+				if(win > moveDie) {
+					
+					win -= moveDie;
+				
+				}
+			
+				else {
+				
+					win = 0;
+				
+				}
+
+				System.out.println("oops!! Player you step on a snake so you will go back to "+moveDie+
+				" step back Or Position will start from O Position");
 			
 			}
 			
 			else {
+
 				win += moveDie;
-				if( win > 100 ) {
+				if( win > WIN_GOAL ) {
 					
 					win -= moveDie;
 				
@@ -67,7 +81,7 @@ class SnakeAndLadder {
 			}
 			
 			// condition to check whether its at 100
-			if( win == 100 ) {
+			if( win == WIN_GOAL ) {
 				
 				System.out.println("Congrats you have Reached 100th Position"); 
 				
