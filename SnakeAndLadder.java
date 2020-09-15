191a3bdfb63d9581c49c29ac5537d8d4e9704a74
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-15 18:28:56 +0530

    [CHELUVESHA]Refactored : followed coding intentions.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,9 +1,9 @@
 // Welcome to Snake And Ladder Game
 import java.util.Random;
 class SnakeAndLadder {
-	final static int SNAKE = 2;
-	final static int NO_PLAY = 0;
-	final static int WIN_GOAL = 100;
+	static final int SNAKE = 2;
+	static final int NO_PLAY = 0;
+	static final int WIN_GOAL = 100;
 	// welcome to starting of the Game	
 	private static void welcomeToGame() {
 		System.out.println("Player will Start from position 0");
